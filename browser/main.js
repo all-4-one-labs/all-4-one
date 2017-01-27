3a8d3c8bc5383eb7b4231f0ee23fe9f70b319eb5
eScribMac <evanscribner1@gmail.com>
2017-01-27 15:37:51 -0500

    refactoring file architecture

--- browser/main.js
+++
@@ -1,9 +0,0 @@
-import create from './create.js';
-import preload from './preload.js';
-import update from './update.js';
-
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-game.config.disableVisibilityChange = true;
-var stage = new Phaser.Stage(game);
-
-export default game;
--- browser/preload.js
+++
@@ -1,9 +0,0 @@
-export default function preload() {
-  this.load.image('ground', 'assets/sky.png');
-  this.load.image('wall', 'assets/platform.png');
-  this.load.spritesheet('dude', 'assets/player-topdown.png', 150, 117);
-  this.load.image('bullet', 'assets/bullet.png');
-  this.load.spritesheet('monster', 'assets/monster-lizard.png', 80 , 56);
-  //this.load.image('button', 'assets/big-red-button.png');
-};
-
