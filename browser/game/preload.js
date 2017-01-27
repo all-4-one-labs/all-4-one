e581f8ab96b5a7d8fc936ea7b23d40d1cba7925a
Richard Shyong <rshyong@gmail.com>
2017-01-27 15:45:46 -0500

    fixed things

--- browser/game/preload.js
+++ browser/game/preload.js
@@ -3,7 +3,7 @@ export default function preload() {
   this.load.image('wall', 'assets/platform.png');
   this.load.spritesheet('dude', 'assets/player-topdown.png', 150, 117);
   this.load.image('bullet', 'assets/bullet.png');
-  this.load.spritesheet('monster', 'assets/monster-lizard.png', 80 , 56);
-  //this.load.image('button', 'assets/big-red-button.png');
+  this.load.spritesheet('monster', 'assets/dinosaur.png', 80 , 56);
+  //this.load.image('button', 'assets/utton.png');
 };
 
