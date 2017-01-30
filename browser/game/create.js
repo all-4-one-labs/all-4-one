4277c819444f9fae52fa548904ed76da9e0a3323
Emily Intersimone <emily.intersimone@gmail.com>
2017-01-30 16:05:09 -0500

    emily's comments

--- browser/game/create.js
+++ browser/game/create.js
@@ -4,42 +4,47 @@ import Bullets from './entities/bullets.js';
 import Wall from './entities/mapObjects.js';
 import socket from '../socket';
 
+// // EI: add these to your game instance as properties?
+// const gameInfo = {
 
-var player;
-var walls;
-var cursors;
-var wasd;
-var fireRate = 400;
-var monsterRate = 1000;
-var button;
-var bullets;
-let id = 0;
+//     var player;
+//     var walls;
+//     var cursors;
+//     var wasd;
+//     var fireRate = 400;
+//     var monsterRate = 1000;
+//     var button;
+//     var bullets;
+//     let id = 0;
+
+// }
+
 let teammates = {} //TODO: on the backend .on('connection'), populate this with existing players instead of waiting for the first interval
 export default function create() {
     var game = this;
 
     //temporary for testing purposes
     //this settings
-    game.world.setBounds(-1000, -1000, 2000, 2000);
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.add.tileSprite(-1000, -1000, 2000, 2000, 'ground');
+    this.world.setBounds(-1000, -1000, 2000, 2000);
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.add.tileSprite(-1000, -1000, 2000, 2000, 'ground');
 
     //walls
-    walls = new Wall(game);
+    this.walls = new Wall(this);
     console.log(socket)
     //player
-    player = new Player(socket.id, game);
+    player = new Player(socket.id, this);
 
 
     //monster
-    // monster = new Monster(id, game);
+    // monster = new Monster(id, this);
 
     //bullets
-    bullets = new Bullets(game);
+    bullets = new Bullets(this);
 
     //button
     //button = this.add.button(this.world.centerX - 95, 400, 'button', spawn, this, 2, 1, 0);
 
 }
 
-export {player, walls, cursors, wasd, fireRate, monsterRate, bullets, teammates};
+export { gameInfo };
