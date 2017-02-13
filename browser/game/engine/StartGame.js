b424c4375970dab964b0fa0b88c132cf6562f84a
jwhorn390 <jwhorn@gmail.com>
2017-02-13 17:17:45 -0500

    CSS changes, cleaned preload file

--- browser/game/engine/StartGame.js
+++ browser/game/engine/StartGame.js
@@ -2,18 +2,9 @@ import create from './create.js';
 import preload from './preload.js';
 import update from './update.js';
 
-let xSize, ySize;
-if ( sessionStorage.getItem('mode') === 'survivor' ) {
-  xSize = 1280;
-  //window.innerwidth
-  ySize = 720;
-  //window.innerheight
-} else if ( sessionStorage.getItem('mode') === 'gamemaster') {
-  xSize = 1280; //3840
-  ySize = 720; //2560
-}
 
-const game = new Phaser.Game(xSize, ySize, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+
+const game = new Phaser.Game(1280, 720, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 game.config.disableVisibilityChange = true;
 
 export default game;
